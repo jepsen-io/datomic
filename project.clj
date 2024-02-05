7997e678a1925d2c3a576a728a63bc29a1b1812c
Kyle Kingsbury <aphyr@aphyr.com>
2024-02-05 09:33:32 -0600

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.datomic "0.1.0-SNAPSHOT"
+(defproject jepsen.datomic "0.1.0"
   :description "Jepsen tests for the Datomic distributed database"
   :url "https://github.com/jepsen-io/datomic"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
